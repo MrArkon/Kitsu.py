89c2e2553bd20066edec34f7f3b951e8e27fbd98
MrArkon <mrarkon@outlook.com>
2021-09-17 23:40:03 +0530

    🔨 Change example

--- examples/search_anime.py
+++ examples/search_anime.py
@@ -4,10 +4,10 @@ import asyncio
 client = kitsu.Client()
 
 async def main():
-    data = await client.search_anime("jujutsu kaisen", limit=1)
+    anime = await client.search_anime("jujutsu kaisen", limit=1)
     
-    print("Canonical Title: " + data.canonical_title)
-    print("Average Rating: " + str(data.average_rating))
+    print("Canonical Title: " + anime.canonical_title)
+    print("Average Rating: " + str(anime.average_rating))
     
     # Close the internal aiohttp ClientSession
     await client.close()
