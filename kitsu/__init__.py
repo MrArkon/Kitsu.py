aa649d9dcf3264e79b59825829ea816c06f7f792
MrArkon <arkon7@outlook.com>
2021-09-20 16:03:27 +0530

    🛠 Bump version to 1.0.0

--- kitsu/__init__.py
+++ kitsu/__init__.py
@@ -8,7 +8,7 @@ A simple async python wrapper for the Kitsu.io API.
 """
 
 __title__   = "kitsu"
-__version__ = "0.2.0"
+__version__ = "1.0.0"
 __author__  = "MrArkon"
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2021-present MrArkon'
