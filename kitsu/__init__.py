4a311879bbf797b66d3247e999df291f04d15459
MrArkon <mrarkon@outlook.com>
2022-05-15 16:08:48 +0530

    🎨 Run black

--- kitsu/__init__.py
+++ kitsu/__init__.py
@@ -7,14 +7,14 @@ A simple async python wrapper for the Kitsu.io API.
 
 """
 
-__title__   = "kitsu"
+__title__ = "kitsu"
 __version__ = "1.0.0"
-__author__  = "MrArkon"
-__license__ = 'MIT'
-__copyright__ = 'Copyright 2021-present MrArkon'
+__author__ = "MrArkon"
+__license__ = "MIT"
+__copyright__ = "Copyright 2021-present MrArkon"
 
-__all__  = ('Client', 'HTTPException', 'BadRequest', 'NotFound', 'Anime', 'Manga')
-__path__ = __import__('pkgutil').extend_path(__path__, __name__)
+__all__ = ("Client", "HTTPException", "BadRequest", "NotFound", "Anime", "Manga")
+__path__ = __import__("pkgutil").extend_path(__path__, __name__)
 
 from logging import NullHandler, getLogger
 
