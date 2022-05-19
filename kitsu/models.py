00915f33ea925f88fb396a0569696bc0c9b35541
MrArkon <mrarkon@outlook.com>
2022-05-19 09:24:55 +0530

    ♻️ Refactored Models | Cut down by 200 lines
    Added a Media baseclass which has the common properties of Anime & Manga

--- kitsu/models.py
+++ kitsu/models.py
@@ -163,20 +163,22 @@ class Title:
         return self._data.get("ja_jp", None)
 
 
-class Anime:
+class Media:
+    """Baseclass for Anime & Manga"""
+
     def __init__(self, payload: dict, session: aiohttp.ClientSession) -> None:
         self._payload: dict = payload
         self._session: aiohttp.ClientSession = session
 
     def __repr__(self) -> str:
-        return f"<Anime id={self.id} title='{self.title}'>"
+        return f"<{self.type.capitalize()} id={self.id} title='{self.title}'>"
 
     def __str__(self) -> Optional[str]:
         return self.title
 
     async def _fetch_categories(self) -> Optional[List[Category]]:
         async with self._session.get(
-            url=f"https://kitsu.io/api/edge/anime/{self.id}/categories", headers=HEADERS
+            url=f"https://kitsu.io/api/edge/{self.type}/{self.id}/categories", headers=HEADERS
         ) as response:
             if response.status == 200:
                 _raw_data = await response.json()
@@ -185,28 +187,17 @@ class Anime:
 
         return [Category(data) for data in _raw_data["data"]]
 
-    async def _fetch_streaming_links(self) -> Optional[List[StreamingLink]]:
-        async with self._session.get(
-            url=f"https://kitsu.io/api/edge/anime/{self.id}/streaming-links", headers=HEADERS
-        ) as response:
-            if response.status == 200:
-                _raw_data = await response.json()
-            else:
-                return None
-
-        return [StreamingLink(data) for data in _raw_data["data"]]
-
     @property
     async def categories(self) -> Optional[List[Category]]:
         return await self._fetch_categories()
 
     @property
-    async def streaming_links(self) -> Optional[List[StreamingLink]]:
-        return await self._fetch_streaming_links()
+    def id(self) -> str:
+        return self._payload.get("id", None)
 
     @property
-    def id(self) -> Optional[str]:
-        return self._payload.get("id", None)
+    def type(self) -> str:
+        return self._payload.get("type", None)
 
     @property
     def created_at(self) -> Optional[datetime]:
@@ -307,10 +298,6 @@ class Anime:
         return self._payload["attributes"].get("ageRatingGuide", None)
 
     @property
-    def subtype(self) -> Optional[Literal["ONA", "OVA", "TV", "movie", "music", "special"]]:
-        return self._payload["attributes"].get("subtype", None)
-
-    @property
     def status(self) -> Optional[Literal["current", "finished", "tba", "unreleased", "upcoming"]]:
         return self._payload["attributes"].get("status", None)
 
@@ -331,6 +318,27 @@ class Anime:
             return self._payload["attributes"]["coverImage"].get(_type, None)
         except AttributeError:
             return None
+
+
+class Anime(Media):
+    async def _fetch_streaming_links(self) -> Optional[List[StreamingLink]]:
+        async with self._session.get(
+            url=f"https://kitsu.io/api/edge/anime/{self.id}/streaming-links", headers=HEADERS
+        ) as response:
+            if response.status == 200:
+                _raw_data = await response.json()
+            else:
+                return None
+
+        return [StreamingLink(data) for data in _raw_data["data"]]
+
+    @property
+    async def streaming_links(self) -> Optional[List[StreamingLink]]:
+        return await self._fetch_streaming_links()
+
+    @property
+    def subtype(self) -> Optional[Literal["ONA", "OVA", "TV", "movie", "music", "special"]]:
+        return self._payload["attributes"].get("subtype", None)
 
     @property
     def episode_count(self) -> Optional[int]:
@@ -355,159 +363,10 @@ class Anime:
         return self._payload["attributes"].get("nsfw", None)
 
 
-class Manga:
-    def __init__(self, payload: dict, session: aiohttp.ClientSession) -> None:
-        self._payload: dict = payload
-        self._session: aiohttp.ClientSession = session
-
-    def __repr__(self) -> str:
-        return f"<Manga id={self.id} title='{self.title}'>"
-
-    def __str__(self) -> Optional[str]:
-        return self.title
-
-    async def _fetch_categories(self) -> Optional[List[Category]]:
-        async with self._session.get(
-            url=f"https://kitsu.io/api/edge/manga/{self.id}/categories", headers=HEADERS
-        ) as response:
-            if response.status == 200:
-                _raw_data = await response.json()
-            else:
-                return None
-
-        return [Category(data) for data in _raw_data["data"]]
-
-    @property
-    async def categories(self) -> Optional[List[Category]]:
-        return await self._fetch_categories()
-
-    @property
-    def id(self) -> str:
-        return self._payload.get("id", None)
-
-    @property
-    def created_at(self) -> Optional[datetime]:
-        try:
-            return isoparse(self._payload["attributes"]["createdAt"])
-        except (KeyError, TypeError):
-            return None
-
-    @property
-    def updated_at(self) -> Optional[datetime]:
-        try:
-            return isoparse(self._payload["attributes"]["updatedAt"])
-        except (KeyError, TypeError):
-            return None
-
-    @property
-    def slug(self) -> Optional[str]:
-        return self._payload["attributes"].get("slug", None)
-
-    @property
-    def synopsis(self) -> Optional[str]:
-        return self._payload["attributes"].get("synopsis", None)
-
-    @property
-    def title(self) -> Optional[Title]:
-        try:
-            return Title(self._payload["attributes"]["titles"])
-        except (KeyError, TypeError):
-            return None
-
-    @property
-    def canonical_title(self) -> Optional[str]:
-        return self._payload["attributes"].get("canonicalTitle", None)
-
-    @property
-    def abbreviated_titles(self) -> Optional[List[str]]:
-        return self._payload["attributes"].get("abbreviatedTitles", None)
-
-    @property
-    def average_rating(self) -> Optional[float]:
-        try:
-            return float(self._payload["attributes"]["averageRating"])
-        except (KeyError, TypeError):
-            return None
-
-    @property
-    def rating_frequencies(self) -> Optional[Dict[str, str]]:
-        return self._payload["attributes"].get("ratingFrequencies", None)
-
-    @property
-    def user_count(self) -> Optional[int]:
-        try:
-            return int(self._payload["attributes"]["userCount"])
-        except (KeyError, TypeError):
-            return None
-
-    @property
-    def favorites_count(self) -> Optional[int]:
-        try:
-            return int(self._payload["attributes"]["favoritesCount"])
-        except (KeyError, TypeError):
-            return None
-
-    @property
-    def start_date(self) -> Optional[datetime]:
-        try:
-            return datetime.strptime(self._payload["attributes"]["startDate"], "%Y-%m-%d")
-        except (KeyError, TypeError):
-            return None
-
-    @property
-    def end_date(self) -> Optional[datetime]:
-        try:
-            return datetime.strptime(self._payload["attributes"]["endDate"], "%Y-%m-%d")
-        except (KeyError, TypeError):
-            return None
-
-    @property
-    def popularity_rank(self) -> Optional[int]:
-        try:
-            return int(self._payload["attributes"]["popularityRank"])
-        except (KeyError, TypeError):
-            return None
-
-    @property
-    def rating_rank(self) -> Optional[int]:
-        try:
-            return int(self._payload["attributes"]["ratingRank"])
-        except (KeyError, TypeError):
-            return None
-
-    @property
-    def age_rating(self) -> Optional[Literal["G", "PG", "R", "R18"]]:
-        return self._payload["attributes"].get("ageRating", None)
-
-    @property
-    def age_rating_guide(self) -> Optional[str]:
-        return self._payload["attributes"].get("ageRatingGuide", None)
-
+class Manga(Media):
     @property
     def subtype(self) -> Optional[Literal["doujin", "manga", "manhua", "manhwa", "novel", "oel", "oneshot"]]:
         return self._payload["attributes"].get("subtype", None)
-
-    @property
-    def status(self) -> Optional[Literal["current", "finished", "tba", "unreleased", "upcoming"]]:
-        return self._payload["attributes"].get("status", None)
-
-    @property
-    def tba(self) -> Optional[str]:
-        return self._payload["attributes"].get("tba", None)
-
-    def poster_image(
-        self, _type: Optional[Literal["tiny", "small", "medium", "large", "original"]] = "original"
-    ) -> Optional[str]:
-        try:
-            return self._payload["attributes"]["posterImage"].get(_type, None)
-        except AttributeError:
-            return None
-
-    def cover_image(self, _type: Optional[Literal["tiny", "small", "large", "original"]] = "original") -> Optional[str]:
-        try:
-            return self._payload["attributes"]["coverImage"].get(_type, None)
-        except AttributeError:
-            return None
 
     @property
     def chapter_count(self) -> Optional[int]:
